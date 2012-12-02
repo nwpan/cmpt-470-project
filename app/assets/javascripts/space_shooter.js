547ecdd982821ee485817897fbd4f682b5f14f84
Nicholas Pan <nwpan@sfu.ca>
2012-12-01 17:28:06 -0800

    space shooter, pew pew

---
+++ app/assets/javascripts/space_shooter.js
@@ -0,0 +1,220 @@
+var scene;
+var playerObject;
+var obstacleModel;
+var obstacles =[];
+
+var projectileModel;
+var projectileObstacles =[];
+
+var jumping  = false;
+var score = 0;
+var life = 3;
+var runningLoop;
+var renderer;
+
+function reset() {
+  alert("Not implemented!");
+}
+
+var collision_detect = function(obj1, obj2) {
+	
+    var hHit = false;
+    var vHit = false;
+
+	distx1 = (obj1.x + obj1.boundWidth);
+	disty1 = (Math.abs(obj1.y) + obj1.boundHeight/2);
+	distx2 = (obj2.x + obj2.boundWidth);
+	disty2 = (Math.abs(obj2.y) + obj2.boundHeight/2);
+	
+	if (obj1.x <= distx2 && obj2.x <= distx1){
+		hHit = true;
+	}	
+	if (Math.abs(obj1.y) <= disty2 && Math.abs(obj2.y) <= disty1){
+		vHit = true;
+	}
+
+    if (hHit == true && vHit == true)
+        return true;
+    
+	return false;
+}
+
+function gameLoop() {
+	//game logic here
+
+  score+= 100;
+  $(".score").html(score);
+
+  if(score % 500 == 0) {
+    if(obstacles.length < 100) {
+      obstacleModel = scene.createObject();
+      obstacleModel.y = Math.floor(Math.random()*8);
+      obstacleModel.x = 100.0;
+      obstacleModel.width = 2.0;
+      obstacleModel.boundWidth = 3.0;
+      obstacleModel.color = [1.0, 1.0, 0.6];
+      obstacleModel.setTexture("assets/crate.jpg");
+      obstacleModel.renderBounds = true;
+      obstacles.push(obstacleModel);
+    }
+  }
+
+  for(var i = 0; i < obstacles.length; i++) {
+  	if(obstacles[i].x <= -20)
+  	{
+      obstacles[i].y = Math.floor(Math.random()*8);
+  	  obstacles[i].x = 100;
+  	}
+    if (!collision_detect(playerObject, obstacles[i]))
+    {
+        obstacles[i].x -= 1;
+        continue;
+    }
+    else 
+    {
+        /*$.ajax({
+          type: 'POST',
+          url: '/high_scores',
+          data: {
+            high_score: score, 
+            game: "Space Shooter"
+          },
+          success: function(data)
+          {
+            alert("POSTED. Check high score page...");
+          }
+        });*/
+        $("#canvas").hide();
+        $("#game-over").removeClass("hidden");
+        $("#status").addClass("hidden").hide();;
+        $(".score").html(score);
+        renderer.stopRender();
+        clearInterval(runningLoop);
+        break;   
+    }
+  }
+
+  for(var i = 0; i < projectileObstacles.length; i++) {
+    for(var o = 0; o < obstacles.length; o++) {
+      if (!collision_detect(obstacles[o], projectileObstacles[i]))
+      {
+          projectileObstacles[i].x += 1;
+          continue;
+      }
+      else
+      {
+        obstacles[o] = 1500;
+        projectileObstacles[i] = -20;
+        continue
+      }
+    }
+  }
+
+	if(playerObject.y < 0 + playerObject.boundHeight/2) {
+		playerObject.y = 0 + playerObject.boundHeight/2;
+    if(playerObject.currentAnimation == 1) {
+      playerObject.boundHeight *= 2;
+      playerObject.animations[1].currentFrame = 0;
+      playerObject.currentAnimation = 0;
+    }
+	}
+}
+
+$(function() {
+
+  $("#play-again").click(function(ev) {
+    reset();
+    runningLoop = setInterval(gameLoop, 1000 / 60);
+    renderer.render();
+    $("#canvas").show();
+    $("#game-over").addClass("hidden");
+    $("#status").removeClass("hidden").show();
+  });
+
+  $(document).keydown(function(e){
+      if (e.keyCode == 38) { //up
+        if(playerObject.y < 8) {
+          playerObject.y += 1.0;
+        }
+         return false;
+      }
+      if (e.keyCode == 40) { //down
+        if(playerObject.y > 1) {
+          playerObject.y -= 1.0;
+        }
+         return false;
+      }
+      if (e.keyCode == 32) { //space
+        projectileModel = scene.createObject();
+        projectileModel.y = playerObject.y;
+        projectileModel.x = playerObject.x;
+        projectileModel.width = 2.0;
+        projectileModel.boundWidth = 3.0;
+        projectileModel.color = [1.0, 1.0, 0.6];
+        projectileModel.setTexture("assets/crate.jpg");
+        projectileModel.renderBounds = true;
+        projectileObstacles.push(projectileModel);
+        return false;
+      }
+      if (e.keyCode == 82) { //reset
+        reset();
+        return false;
+      }
+  });
+  
+  renderer = new Renderer($('#canvas')[0]);
+
+
+  $('#loading').hide();
+
+  scene = renderer.createScene();
+
+  scene.camera([0, 4, 10.0],
+      [0.0, 4, -4.0],
+      [0.0, 1.0, 0.0]);
+  
+
+  playerObject = scene.createObject();
+  playerObject.x = -8;
+  playerObject.y = 2.0;
+  playerObject.rotateY = 100;
+  playerObject.boundHeight = 0.5;
+  playerObject.height = 0.5;
+  playerObject.renderBounds = true;
+  playerObject.loadModelFromJson("/run/charrun1", "assets/char.jpg");
+
+  /*
+  var run = playerObject.createAnimation();
+  for(var i = 1; i <= 13; i++ ) {
+    run.addFrameFromJson("/run/charrun" + i.toString(), "assets/char.jpg");
+  }
+  run.speed = 0.5;
+  */
+
+
+  var floorObject = scene.createObject();
+  floorObject.y = 0;
+  floorObject.width = 1500.0;
+  floorObject.depth = 2.0;
+  floorObject.boundWidth = 1500.0;
+  floorObject.boundDepth = 2.0;
+  floorObject.color = [1, 1, 1];
+
+  obstacleModel = scene.createObject();
+  obstacleModel.y =  Math.floor(Math.random()*11);
+  obstacleModel.x = 300.0;
+  obstacleModel.width = 2.0;
+  obstacleModel.boundWidth = 3.0;
+  obstacleModel.color = [1.0, 1.0, 0.6];
+  obstacleModel.setTexture("assets/crate.jpg");
+
+  obstacles.push(obstacleModel);
+
+  runningLoop = setInterval(gameLoop, 1000 / 60);
+
+  $("#status").removeClass("hidden").show();
+  renderer.render();
+
+  return true;
+
+});
