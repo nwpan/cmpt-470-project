432d4584b11b371dc146045033b770908a65bb65
Nicholas Pan <nwpan@sfu.ca>
2012-12-04 14:46:40 -0800

    adding db:reset for seeds

--- dev_scripts/deploy.sh
+++ dev_scripts/deploy.sh
@@ -6,6 +6,7 @@ bundle install
 rake db:drop
 rake db:create
 rake db:migrate
+rake db:reset
 rake assets:precompile
 sudo touch tmp/restart.txt
 echo "Master branch pushed to production server, please check http://cmpt470.csil.sfu.ca:8001!"
