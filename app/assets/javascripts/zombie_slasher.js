9c668b94c237b8d6196f496e9ac96eaf3640678c
Jordon Phillips <jordonph@gmail.com>
2012-12-05 21:00:09 -0800

    finished first version of zombie slasher

--- app/assets/javascripts/zombie_slasher.js
+++ app/assets/javascripts/zombie_slasher.js
@@ -1,74 +1,144 @@
 var testScene;
 var playerObject;
-var obstacleModel;
-var obstacles =[];
-var jumping  = false;
+var attack  = false;
 var score = 0;
-var life = 3;
 var runningLoop;
 var renderer;
-var zombie;
+var zombies = [];
+var timer = 0;
+var swordCollide = {x: 0, y: 0, z: 1.5, boundHeight: 1.0, boundWidth: 2.0, boundDepth: 2.0};
 
 function reset() {
   score = 0;
-  obstacles = [];
-  jumping = false;
-  score = 0;
-  playerObject.z = -4.0;
-  testScene.objects = [testScene.objects[0], testScene.objects[1], testScene.objects[2], testScene.objects[3]];
-  obstacleModel = testScene.createObject();
-  obstacleModel.z = Math.floor(Math.random()*3)*2-6;
-  obstacleModel.x = 300.0;
-  obstacleModel.color = [1.0, 1.0, 0.6];
-  obstacleModel.setTexture("assets/crate.jpg");
-  testScene.objects.push(obstacleModel);
-  obstacles.push(obstacleModel);
+  $(".score").html(score);
+  zombies = [];
+  attack = false;
+  playerObject.z = 0;
+  playerObject.x = 0;
+  testScene.objects = [testScene.objects[0], testScene.objects[1]];
 }
 
+var generateZombie = function() {
+  var zombie = testScene.createAvatar($('#user_id').val());
+  if(Math.random() > 0.5) {
+    if(Math.random() > 0.5) {
+      zombie.x = 15;
+    }else {
+      zombie.x = -15;
+    }
+    zombie.z = Math.random()*14 - 7;
+  } else {
+    zombie.x = Math.random()*30 - 15;
+    if(Math.random() > 0.5) {
+      zombie.z = 12;
+    }else {
+      zombie.z = -12;
+    }
+  }
+  zombie.boundHeight = 1.3;
+  var zombieRun = new Animation(252, 351);
+  zombieRun.speed = 0.8;
+  zombie.animations.push(zombieRun);
+  zombie.skinColor = [0.7, 1.0, 0.5];
+  zombies.push(zombie);
+};
+
 var collision_detect = function(obj1, obj2) {
-	
-    var hHit = false;
-    var vHit = false;
-
-	distx1 = (obj1.x + obj1.boundWidth);
-	disty1 = 2*(Math.abs(obj1.y) + obj1.boundHeight/2);
-	distx2 = (obj2.x + obj2.boundWidth);
-	disty2 = 2*(Math.abs(obj2.y) + obj2.boundHeight/2);
-	
-	if (obj1.x <= distx2 && obj2.x <= distx1){
-		hHit = true;
-	}	
-	if (Math.abs(obj1.y) <= disty2 && Math.abs(obj2.y) <= disty1){
-		vHit = true;
-	}
-
-    if (hHit == true && vHit == true)
+	if(obj1.y == obj2.y) {
+    if(obj2.x < obj1.x + obj1.boundWidth/2 && obj2.x > obj1.x - obj1.boundWidth/2) {
+      if(obj2.z < obj1.z + obj1.boundDepth/2 && obj2.z > obj1.z - obj1.boundDepth/2) {
         return true;
-    
-	return false;
+      }
+    }
+  }
+  return false;
 }
 
 function gameLoop() {
 	//game logic here
+  if(timer % 600 === 0) {
+    if(zombies.length < 30) {
+      generateZombie();
+    }
+  }
+  timer++;
+
   if(playerObject.currentAnimation == 2 && playerObject.animations[playerObject.currentAnimation].isComplete()) {
+    attack = false;
     playerObject.changeAnimation(0);
   }
 
-  if(zombie.x < playerObject.x*1.2) {
-    zombie.rotateY = 90;
-    zombie.x += 0.01;
-  }
-  if(zombie.x > playerObject.x*1.2) {
-    zombie.rotateY = -90;
-    zombie.x -= 0.01;
-  }
-  if(zombie.z < playerObject.z*1.2) {
-    zombie.rotateY = 0;
-    zombie.z += 0.01;
-  }
-  if(zombie.z > playerObject.z*1.2) {
-    zombie.rotateY = 180;
-    zombie.z -= 0.01;
+  for(var zombie in zombies) {
+    zombie = zombies[zombie];
+
+    if(attack && collision_detect(zombie, swordCollide) && playerObject.animations[playerObject.currentAnimation].currentFrame > 10) {
+      score += 100;
+      $(".score").html(score);
+      zombie.y = -100;
+    }
+
+    if(collision_detect(zombie, playerObject)) {
+      $.ajax({
+          type: 'POST',
+          url: '/high_scores',
+          data: {
+            high_score: score, 
+            game: "Zombie Slasher"
+          },
+          success: function(data)
+          {
+            
+          }
+        });
+        $("#canvas").hide();
+        $("#game-over").removeClass("hidden");
+        $("#status").addClass("hidden").hide();
+        $(".score").html(score);
+        renderer.stopRender();
+        clearInterval(runningLoop);
+        break; 
+    }
+
+    var zombieCollide = false;
+    for(var otherZombie in zombies) {
+      otherZombie = zombies[otherZombie];
+      if(otherZombie != zombie && collision_detect(zombie, otherZombie)) {
+        zombieCollide = otherZombie;
+        break;
+      }
+    }
+
+    if(!zombieCollide) {
+      var angle = Math.atan2((playerObject.x - zombie.x), (playerObject.z - zombie.z)) * (180/Math.PI);
+      zombie.rotateY = angle;
+      if(zombie.x < playerObject.x) {
+        zombie.x += 0.01;
+      }
+      if(zombie.x > playerObject.x) {
+        zombie.x -= 0.01;
+      }
+      if(zombie.z < playerObject.z) {
+        zombie.z += 0.01;
+      }
+      if(zombie.z > playerObject.z) {
+        zombie.z -= 0.01;
+      }
+    } else {
+      var angle = Math.atan2((zombie.x - zombieCollide.x), (zombie.z - zombieCollide.z)) * (180/Math.PI);
+      zombie.rotateY = angle;
+      if(zombie.x < zombieCollide.x) {
+        zombie.x -= 0.01;
+      }
+      if(zombie.x > zombieCollide.x) {
+        zombie.x += 0.01;
+      }
+      if(zombie.z < zombieCollide.z) {
+        zombie.z -= 0.01;
+      }
+      if(zombie.z > zombieCollide.z) {
+        zombie.z += 0.01;
+      }
+    }
   }
 
 }
@@ -85,46 +155,58 @@ $(function() {
   });
 
   $(document).keydown(function(e){
-      console.log(playerObject.x + "," + playerObject.z);
-      if (e.keyCode == 37) { //left
-          playerObject.rotateY = 180;
-          if(playerObject.z > -12) {
-            playerObject.z-= 0.7;
-          }
-      }
-      if (e.keyCode == 39) { //right
-          playerObject.rotateY = 0;
-          if(playerObject.z < 12) {
-            playerObject.z+= 0.7;
-          }
-      }
-      if (e.keyCode == 40) { //down
-          playerObject.rotateY = -90;
-          if(playerObject.x > -6) {
-            playerObject.x-= 0.7;
-          }
-      }
-      if (e.keyCode == 38) { //up
-          playerObject.rotateY = 90;
-          if(playerObject.x < 7) {
-            playerObject.x+= 0.7;
-          }
-      }
-      if (e.keyCode == 37 || e.keyCode == 39 || e.keyCode == 40 || e.keyCode == 38) { 
-        if(playerObject.currentAnimation != 1) {
-          playerObject.changeAnimation(1);
+      if(!attack) {
+        if (e.keyCode == 37) { //left
+            playerObject.rotateY = 180;
+            if(playerObject.z > -12) {
+              playerObject.z-= 0.7;
+            }
+            swordCollide.x = playerObject.x;
+            swordCollide.z = playerObject.z - 1.5;
         }
-      }
+        if (e.keyCode == 39) { //right
+            playerObject.rotateY = 0;
+            if(playerObject.z < 12) {
+              playerObject.z+= 0.7;
+            }
+            swordCollide.x = playerObject.x;
+            swordCollide.z = playerObject.z + 1.5;
+        }
+        if (e.keyCode == 40) { //down
+            playerObject.rotateY = -90;
+            if(playerObject.x > -6) {
+              playerObject.x-= 0.7;
+            }
+            swordCollide.x = playerObject.x - 1.5;
+            swordCollide.z = playerObject.z;
+        }
+        if (e.keyCode == 38) { //up
+            playerObject.rotateY = 90;
+            if(playerObject.x < 7) {
+              playerObject.x+= 0.7;
+            }
+            swordCollide.x = playerObject.x + 1.5;
+            swordCollide.z = playerObject.z;
+        }
+        if (e.keyCode == 37 || e.keyCode == 39 || e.keyCode == 40 || e.keyCode == 38) { 
+          if(playerObject.currentAnimation != 1) {
+            playerObject.changeAnimation(1);
+          }
+        }
 
-      if (e.keyCode == 32) { //space
+        if (e.keyCode == 32) { //space
+            attack = true;
             playerObject.changeAnimation(2);
+        }
       }
   });
 
   $(document).keyup(function(e){
+    if(!attack) {
       if (e.keyCode == 37 || e.keyCode == 39 || e.keyCode == 40 || e.keyCode == 38) { 
         playerObject.changeAnimation(0);
       }
+    }
   });
   
   renderer = new Renderer($('#canvas')[0]);
@@ -141,10 +223,9 @@ $(function() {
   var idle = new Animation(109, 160);
   var run = new Animation(162, 211);
   run.speed = 0.7;
-  var slash = new Animation(214, 245);
+  var slash = new Animation(220, 245);
+  slash.speed = 1.1;
   slash.loop = false;
-  var zombieRun = new Animation(252, 351);
-  zombieRun.speed = 0.8;
   
 
   playerObject = testScene.createAvatar($('#user_id').val());
@@ -152,14 +233,7 @@ $(function() {
   playerObject.animations.push(idle);
   playerObject.animations.push(run);
   playerObject.animations.push(slash);
-  //playerObject.skinColor = [0.7, 1.0, 0.5];
   playerObject.loadPropFromAjax("sword", "hand_r");
-
-  zombie = testScene.createAvatar($('#user_id').val());
-  zombie.boundHeight = 1.3;
-  zombie.animations.push(zombieRun);
-  zombie.skinColor = [0.7, 1.0, 0.5];
-
 
 
   var testObject3 = testScene.createObject();
@@ -171,7 +245,6 @@ $(function() {
   testObject3.boundDepth = 500.0;
   testObject3.color = [0.5, 0.5, 0.5];
 
-  obstacles.push(obstacleModel);
 
   runningLoop = setInterval(gameLoop, 1000 / 60);
 
